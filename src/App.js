2ea821b26dda28e2b1667d9b707d27a21d2c6f06
FGonzalezLeiva <f.gonzalesdvfdb>
2022-05-04 17:59:29 -0400

    versión registrar cambio

--- src/App.js
+++ src/App.js
@@ -1,13 +1,23 @@
 import logo from './logo.svg';
 import './App.css';
+// {Mapa} from './views/mapa'
+
+const Mapa =()=> {
+  return (
+    <div>
+        hola
+    </div>
+  );
+}
 
 function App() {
+  //<Mapa />
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Edit <code>src/App.js</code> guardado.
         </p>
         <a
           className="App-link"
---
+++ src/views/mapa.js
@@ -0,0 +1,12 @@
+import React from 'react';
+//import ReactDOM from 'react-dom/client';
+
+const Mapa =()=> {
+  return (
+    <div>
+        hola
+    </div>
+  );
+}
+
+export default Mapa;
