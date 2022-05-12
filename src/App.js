bc00cd8adc260d6e689e8c978c74a1318e5b497c
FGonzalezLeiva <101906075+FGonzalezLeiva@users.noreply.github.com>
2022-05-11 20:17:21 -0400

    Update App.js

--- src/App.js
+++ src/App.js
@@ -11,7 +11,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Valeria te amo
+          Valeria no te amo
         </p>
         <a
           className="App-link"
